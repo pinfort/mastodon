7f667c20cb5667acffde50c9bda4382b8a69b3ab
pinfort <pinfo.walter@gmail.com>
2017-05-03 16:16:35 +0900

    add area field to database

---
+++ db/migrate/20170424131420_add_area_field_to_accounts.rb
@@ -0,0 +1,6 @@
+class AddAreaFieldToAccounts < ActiveRecord::Migration[5.0]
+  def change
+    add_column :accounts, :area, :integer, null: false, default: 0
+    add_index :accounts, [:area], unique: false
+  end
+end
