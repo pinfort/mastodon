8e1f05107e9b6318e619b9e1209421e85209024b
pinfort <ptg@nijitei.com>
2020-05-04 01:25:26 +0900

    update validation

--- app/models/account.rb
+++ app/models/account.rb
@@ -80,7 +80,7 @@ class Account < ApplicationRecord
   # Local user validations
   validates :username, format: { with: /\A[a-z0-9_]+\z/i }, length: { maximum: 30 }, if: -> { local? && will_save_change_to_username? && actor_type != 'Application' }
   validates_with UnreservedUsernameValidator, if: -> { local? && will_save_change_to_username? }
-  validates :area, numericality: true, if: -> { local? }
+  validates :area, numericality: true, if: -> { local? && will_save_change_to_area? }
   validates :display_name, length: { maximum: 30 }, if: -> { local? && will_save_change_to_display_name? }
   validates :note, note_length: { maximum: 500 }, if: -> { local? && will_save_change_to_note? }
   validates :fields, length: { maximum: 4 }, if: -> { local? && will_save_change_to_fields? }
