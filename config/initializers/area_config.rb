a4e25c7ec499a881a37b0944928b3bcefd587315
pinfort <ptg@nijitei.com>
2023-09-25 23:01:57 +0900

    lint

--- config/initializers/area_config.rb
+++ config/initializers/area_config.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
-json_file_path = "#{Rails.root}/app/javascript/area_settings.json"
-area_settings = open(json_file_path) do |io|
-  JSON.load(io)
+
+json_file_path = Rails.root.join("app/javascript/area_settings.json")
+area_settings = File.open(json_file_path) do |f|
+  JSON.parse(f)
 end
-Rails.application.config.account_area = area_settings["areas"]
-Rails.application.config.instances_area = area_settings["instance-areas"]
+Rails.application.config.account_area = area_settings['areas']
+Rails.application.config.instances_area = area_settings['instance-areas']
 
 areaHash = {}
 Rails.application.config.instances_area.each do |area|
