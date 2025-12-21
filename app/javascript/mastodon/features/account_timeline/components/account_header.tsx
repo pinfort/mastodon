4e5473e67dc54fc8c78dbf9eba5acfd63aa49303
pinfort <ptg@nijitei.com>
2025-12-21 17:10:00 +0900

    header

--- app/javascript/mastodon/features/account_timeline/components/account_header.tsx
+++ app/javascript/mastodon/features/account_timeline/components/account_header.tsx
@@ -29,6 +29,7 @@ import {
 import { openModal } from 'mastodon/actions/modal';
 import { initMuteModal } from 'mastodon/actions/mutes';
 import { initReport } from 'mastodon/actions/reports';
+import Area_header from 'mastodon/components/area_header';
 import { Avatar } from 'mastodon/components/avatar';
 import { Badge, AutomatedBadge, GroupBadge } from 'mastodon/components/badge';
 import { Button } from 'mastodon/components/button';
@@ -62,6 +63,7 @@ import { useAppSelector, useAppDispatch } from 'mastodon/store';
 import { FamiliarFollowers } from './familiar_followers';
 import { MemorialNote } from './memorial_note';
 import { MovedNote } from './moved_note';
+
 
 const messages = defineMessages({
   unblock: { id: 'account.unblock', defaultMessage: 'Unblock @{name}' },
@@ -844,6 +846,7 @@ export const AccountHeader: React.FC<{
                 account={suspended || hidden ? undefined : account}
                 size={92}
               />
+              <Area_header account={account} />
             </a>
 
             <div className='account__header__tabs__buttons'>
