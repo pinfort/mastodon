cedd32f31c84adee7ec7fd29a7d3cb49c627315a
pinfort <ptg@nijitei.com>
2025-12-30 16:04:32 +0900

    fix reassignment

--- config/initializers/area_config.rb
+++ config/initializers/area_config.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-json_file_path = Rails.root.join('app', 'javascript', 'hyogo-areas.json')
-Rails.application.config.account_area = File.open(json_file_path) do |f|
+hyogo_areas_path = Rails.root.join('app', 'javascript', 'hyogo-areas.json')
+Rails.application.config.account_area = File.open(hyogo_areas_path) do |f|
   JSON.parse(f.read)['areas']
 end
 
-json_file_path = Rails.root.join('app', 'javascript', 'area-timelines.json')
-timelines = File.open(json_file_path) do |f|
+area_timelines_path = Rails.root.join('app', 'javascript', 'area-timelines.json')
+timelines = File.open(area_timelines_path) do |f|
   JSON.parse(f.read)
 end
 Rails.application.config.instances_area_hash = timelines.transform_values { |val| val['instances'] }
