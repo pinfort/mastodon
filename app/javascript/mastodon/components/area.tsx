491aa933e1580880b21f20191c57315fc9b34835
pinfort <ptg@nijitei.com>
2026-03-18 23:41:38 +0900

    private

--- app/javascript/mastodon/components/area.tsx
+++ app/javascript/mastodon/components/area.tsx
@@ -27,8 +27,8 @@ interface Props {
 }
 
 class Area extends React.PureComponent<Props> {
-  config: Record<AreaId, AreaConfig>;
-  instances: Record<InstanceDomain, InstanceConfig>;
+  private config: Record<AreaId, AreaConfig>;
+  private instances: Record<InstanceDomain, InstanceConfig>;
 
   constructor(props: Props) {
     super(props);
@@ -38,7 +38,7 @@ class Area extends React.PureComponent<Props> {
     );
   }
 
-  get_area_eng_name(account: Account): string {
+  protected get_area_eng_name(account: Account): string {
     if (this.is_local(account)) {
       return this.get_local_area_eng_name(account.area);
     } else {
@@ -46,11 +46,11 @@ class Area extends React.PureComponent<Props> {
     }
   }
 
-  get_local_area_eng_name(area_id: AreaId): string {
+  private get_local_area_eng_name(area_id: AreaId): string {
     return this.getFromConfigOrDefault(area_id)['area-eng-name'];
   }
 
-  get_remote_area_eng_name(account: Account): string {
+  private get_remote_area_eng_name(account: Account): string {
     const domain = account.acct.split('@').at(-1) ?? '';
     return (
       this.instances[domain]?.['instance-eng-name'] ??
@@ -58,7 +58,7 @@ class Area extends React.PureComponent<Props> {
     );
   }
 
-  get_area_short_name(account: Account): string {
+  protected get_area_short_name(account: Account): string {
     if (this.is_local(account)) {
       return this.get_local_area_short_name(account.area);
     } else {
@@ -66,11 +66,11 @@ class Area extends React.PureComponent<Props> {
     }
   }
 
-  get_local_area_short_name(area_id: AreaId): string {
+  private get_local_area_short_name(area_id: AreaId): string {
     return this.getFromConfigOrDefault(area_id)['area-short-name'];
   }
 
-  get_remote_area_short_name(account: Account): string {
+  private get_remote_area_short_name(account: Account): string {
     const domain = account.acct.split('@').at(-1) ?? '';
     return (
       this.instances[domain]?.['instance-short-name'] ??
@@ -78,7 +78,7 @@ class Area extends React.PureComponent<Props> {
     );
   }
 
-  getFromConfigOrDefault(key: AreaId): AreaConfig {
+  private getFromConfigOrDefault(key: AreaId): AreaConfig {
     // 存在しない場合は0にフォールバック。0は未設定の値が入っている
     return (
       this.config[key] ??
@@ -89,7 +89,7 @@ class Area extends React.PureComponent<Props> {
     );
   }
 
-  is_local(account: Account): boolean {
+  private is_local(account: Account): boolean {
     return account.username === account.acct;
   }
 }
